c10f092785e0079a4dff164cf8c36d89a5aed855
Marcelo Baldini <mbaldini@finnegans.com.ar>
2021-10-21 17:58:10 -0300

    Clase 17 completa: Rutas

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app =express();
 
 //Cargamos rutas
+var user_routes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -13,9 +14,7 @@ app.use(bodyParser.json());
 // configurar cabeceras http
 
 // rutas base
+app.use('/api', user_routes);
 
-app.get('/pruebas', function(req,res){
-    res.status(200).send({message: 'Bienvenido a mi api rest con node'});
-});
 
 module.exports =app;
