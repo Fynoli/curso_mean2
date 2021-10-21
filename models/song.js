ff44d544a5a815180a1cf8f8b8301f73b0b5aa9b
Marcelo Baldini <mbaldini@finnegans.com.ar>
2021-10-21 16:32:49 -0300

    Modelos completos

---
+++ models/song.js
@@ -0,0 +1,14 @@
+'use strict' //obliga características modernas
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SongSchema = Schema({
+    number: String,
+    name: String,
+    duration: String,
+    file: String,
+    album: {type: Schema.ObjectId, ref: 'Album'}
+});
+
+module.exports = mongoose.model('Song', SongSchema)
